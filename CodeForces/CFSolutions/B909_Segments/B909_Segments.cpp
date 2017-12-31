af1aba01e5f17f1b10af38ed9fbfde5dbee2a83b
Md. Belal Hossain <30633331+belal-bh@users.noreply.github.com>
2017-12-31 22:48:26 +0600

    B909_Segments.cpp solved

---
+++ CodeForces/CFSolutions/B909_Segments/B909_Segments.cpp
@@ -0,0 +1,19 @@
+/*
+* CF: B909_Segments
+* http://codeforces.com/contest/909/problem/B
+* belal_bh
+* 31.12.17
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int min_lyr = (n/2+1)*((n+1)/2);
+    printf("%d",min_lyr);
+    return 0;
+}
