c5d37910c4f0883a4bd3fcfd74340490e378363c
Md. Belal Hossain <30633331+belal-bh@users.noreply.github.com>
2018-01-03 16:56:03 +0600

    UVa 10071 - Back to High School problem's solution
    /*
     * UVa 10071 - Back to High School
     * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1012
     * Root :: Contest Volumes (10000...) :: Volume 112 (11200-11299) * belal_bh
     * 03.01.2018
     */

---
+++ UVa/UVaSolutions/UVa10071_Back_to_High_School/UVa10071_Back_to_High_School.cpp
@@ -0,0 +1,18 @@
+/*
+ * UVa 10071 - Back to High School
+ * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1012
+ * Root :: Contest Volumes (10000...) :: Volume 112 (11200-11299) * belal_bh
+ * 03.01.2018
+ */
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    long long int v,t;
+    while(scanf("%lld%lld",&v,&t)!=EOF){
+       printf("%lld\n",(2*v*t));
+    }
+    return 0;
+}
+
