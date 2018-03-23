bf5af3030e06f3634b55d07c07113bcd9afbdf1a
Md. Belal Hossain <30633331+belal-bh@users.noreply.github.com>
2018-03-23 17:39:20 +0600

    Update and rename UVa/UVaSolutions/UVa-p272_TEX_Quotes/UVa-p272_TEX_Quotes.cpp to UVa/UVaSolutions/272-TEX Quotes/272	TEX Quotes.cpp

--- UVa/UVaSolutions/272-TEX Quotes/272	TEX Quotes.cpp
+++ UVa/UVaSolutions/272-TEX Quotes/272	TEX Quotes.cpp
@@ -1,11 +1,3 @@
-/*
- * UVa 272 - TEX Quotes
- * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=121&page=show_problem&problem=208
- * Root :: Competitive Programming: Increasing the Lower Bound of Programming Contests (Steven & Felix Halim) :: Chapter 1. Introduction :: Ad Hoc
- * belal_bh
- * 27.12.17
-*/
-
 #include<bits/stdc++.h>
 
 using namespace std;
