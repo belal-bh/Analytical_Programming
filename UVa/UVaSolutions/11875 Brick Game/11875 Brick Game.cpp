30678f0f649eb6c46d50f6942bc38e7afd71e9a2
Md. Belal Hossain <30633331+belal-bh@users.noreply.github.com>
2018-03-28 01:52:15 +0600

    Add files via upload

---
+++ UVa/UVaSolutions/11875 Brick Game/11875 Brick Game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace  std;
+int main(){
+    int T;
+    cin >> T;
+    int n, c;
+    for (int t = 1; t <= T; ++t){
+        cin >> n;
+        for (int i = 0; i < n; ++i){
+            cin >> c;
+            if (i == n / 2)
+                cout << "Case " << t << ": " << c << '\n';
+        }
+    }
+    return 0;
+}
